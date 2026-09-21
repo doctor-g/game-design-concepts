de02269eb01dce692c70297e1669367daefd4b72
Paul Gestwicki <pvgestwicki@bsu.edu>
2026-09-21 10:20:38 -0400

    Configure base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://doctor-g.github.io',
+  base: '/game-design-concepts',
+});
