34bf5f60739607759eb84c4f17da110fcca0db01
Paul Gestwicki <pvgestwicki@bsu.edu>
2026-09-21 12:04:17 -0400

    Secure hypertext links in Markdown sources

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { unified } from "@astrojs/markdown-remark";
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://doctor-g.github.io',
   base: '/game-design-concepts',
+  markdown: {
+    processor: unified({
+      rehypePlugins: [
+        // For markdown links, make them safer.
+        [
+          rehypeExternalLinks,
+          {
+            target: "_blank",
+            rel: ["noopener", "noreferrer"],
+          },
+        ],
+      ],
+    }),
+  }
 });
