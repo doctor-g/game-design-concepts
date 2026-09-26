a87df529ce7cd1704becc9fcaa1778abb4fdc35b
Paul Gestwicki <pvgestwicki@bsu.edu>
2026-09-26 11:39:42 -0400

    Copy text of level 1
    This also required setting up a shiki theme so that the grid of numbers in the
    Three to Fifteen example shows up properly.
    
    The intro to this level has been removed because it is about Schreiber's online teaching
    experiment. The homeplay still refers to days of the week and items that may not
    exist any more; more work may be required to make this sensible to new audiences.
    For example, an intro page should explain that the contents have been lightly edited
    and what books he assigned.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,12 @@ export default defineConfig({
   site: 'https://doctor-g.github.io',
   base: '/game-design-concepts',
   markdown: {
+    shikiConfig: {
+      // This theme is used only for laying out data in a grid.
+      // The actual theme contains underlines that violate the WCAG requirements
+      // and so showing source code would require a hack in the main CSS file.
+      theme: 'github-light-high-contrast',
+    },
     processor: unified({
       rehypePlugins: [
         // For markdown links, make them safer.
